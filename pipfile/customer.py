a176bd42050f5e8b1f8816ae100ce5077dfafcc3
Ryan <ryan.kiprop@student.moringaschool.com>
2025-05-18 00:43:26 +0300

    implemented customer class

---
+++ pipfile/customer.py
@@ -0,0 +1,27 @@
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if not 1 <= len(value) <= 15:
+            raise ValueError("Name must be between 1 and 15 characters")
+        self._name = value
+
+    def orders(self):
+        return self._orders
+
+    def coffees(self):
+        return list({order.coffee for order in self._orders})
+
+    def create_order(self, coffee, price):
+        new_order = Order(self, coffee, price)
+        self._orders.append(new_order)
+        return new_order
